afd2da7e3942b2bb65077432509a9d3314f2c75a
Andrey Minin <andrey.m.tech@gmail.com>
2023-08-16 15:17:01 +0300

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Andrey Minin on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
